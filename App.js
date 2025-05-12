81d295224d110fdab6189c7a0bd811143da6f602
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-05-12 17:04:52 -0500

    Feature: Render restaurant cards dynamically using props and map()
    - Added an array of restaurant objects
    - Used .map() to render RestaurantCard components dynamically
    - Passed data to cards using props
    - Laying foundation for future features like search and filtering

--- App.js
+++ App.js
@@ -25,30 +25,62 @@ const Header = () => {
 };
 
 // RestaurantCard
-const RestaurantCard = () => {
+const RestaurantCard = ({ name, cuisines, rating, eta, image }) => {
     return (
         <div className="res-card">
-            <img
-                className="res-logo"
-                src="https://assets.bonappetit.com/photos/61ba78e6ca92b3cc74fd3299/master/w_1600,c_limit/THB.jpg"
-                alt="res-logo"
-            />
-            <h3>Meghana Foods</h3>
-            <h4>Biryani, North Indian, Asian</h4>
-            <h4>4.5 Stars</h4>
-            <h4>38 minutes</h4>
+            <img className="res-logo" src={image} alt="res-logo" />
+            <h3>{name}</h3>
+            <h4>{cuisines}</h4>
+            <h4>{rating}</h4>
+            <h4>{eta}</h4>
         </div>
     );
 };
 
 // Body Section
 const Body = () => {
+    // Array of restaurant dummy data
+    const restaurantList = [
+        {
+            name: 'Meghana Foods',
+            cuisines: 'Biryani, North Indian, Asian',
+            rating: '4.5 Stars',
+            eta: '38 minutes',
+            image: 'https://assets.bonappetit.com/photos/61ba78e6ca92b3cc74fd3299/master/w_1600,c_limit/THB.jpg',
+        },
+        {
+            name: 'KFC',
+            cuisines: 'Fried Chicken, Burgers',
+            rating: '4.2 Stars',
+            eta: '25 minutes',
+            image: 'https://assets.bonappetit.com/photos/61ba78e6ca92b3cc74fd3299/master/w_1600,c_limit/THB.jpg',
+        },
+        {
+            name: "Domino's Pizza",
+            cuisines: 'Pizza, Italian',
+            rating: '4.3 Stars',
+            eta: '30 minutes',
+            image: 'https://assets.bonappetit.com/photos/61ba78e6ca92b3cc74fd3299/master/w_1600,c_limit/THB.jpg',
+        },
+        // Later add 3 more below to make it 6 total
+    ];
+
     return (
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
                 {/* // RestaurantCard */}
-                <RestaurantCard />
+
+                {restaurantList.map((restaurant, index) => (
+                    <RestaurantCard
+                        key={index}
+                        name={restaurant.name}
+                        cuisines={restaurant.cuisines}
+                        rating={restaurant.rating}
+                        eta={restaurant.eta}
+                        image={restaurant.image}
+                    />
+                ))}
             </div>
         </div>
     );
